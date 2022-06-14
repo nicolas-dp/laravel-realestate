4452f325c9bc2d795bda057854391617b898b8ab
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-06-14 12:50:02 +0200

    first commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel @yield('page-title', 'RealEstate')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body>
+    @include('partials.header')
+    <main>
+        @yield('content')
+    </main>
+    <footer></footer>
+
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.app')
+
+@section('content')
+
+<div class="p-5 bg-light">
+    <div class="container">
+        <h1 class="display-3">Real Estate Agency</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <hr class="my-2">
+       
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Visit Properties</a>
+        </p>
+    </div>
+</div>
+
+<section class="container">
+
+<h3>Our properties</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus veritatis beatae assumenda laboriosam illo eos perferendis maiores modi nihil consequatur, maxime fugiat. Similique quis odio et tempora perferendis ex minus.</p>
+
+</section>
+
+@stop
